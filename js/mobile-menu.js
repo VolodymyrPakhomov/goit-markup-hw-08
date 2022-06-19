df9fe6d5678da4d98897ce54513680ae91c9504a
VolodymyrPakhomov <vova.zest997@gmail.com>
2022-06-20 02:08:45 +0300

    2
    2

--- js/mobile-menu.js
+++ js/mobile-menu.js
@@ -1,7 +1,8 @@
 (() => {
     const menuBtnRef = document.querySelector("[data-menu-button]");
     const mobileMenuRef = document.querySelector("[data-menu]");
-  
+
+
     menuBtnRef.addEventListener("click", () => {
       const expanded =
         menuBtnRef.getAttribute("aria-expanded") === "true" || false;
